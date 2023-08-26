87de476ef2b6d5e770f918b0e99e6b4854ccd8a8
HelloitsMyusernameiscodeing <107098436+HelloitsMyusernameiscodeing@users.noreply.github.com>
2023-08-26 10:38:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+tree=""
+function preload(){
+tree=loadImage("OIP.jpeg")
+}
+function setup(){
+canvas=createCanvas(700,600)
+}
+function draw(){
+fill("cyan")
+stroke("black")
+circle(100,100,100,)
+fill("yellow")
+stroke("black")
+rect(150,100,400,20)
+fill("red")
+stroke("black")
+circle(600,100,100)
+fill("green")
+stroke("black")
+rect(590,150,20,300)
+fill("blue")
+stroke("black")
+circle(599,500,100)
+fill("purple")
+stroke("black")
+rect(150,500,400,20)
+fill("pink")
+stroke("black")
+circle(100,500,100)
+fill("burlywood")
+stroke("black")
+rect(90,150,20,300)
+image(tree,350,300,90,90)
+}
